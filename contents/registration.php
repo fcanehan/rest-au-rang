a3bd1704e00bb663cf2fb9a9c27eb88d1c45e39a
fabien <fabien.canehan@gmail.com>
2018-05-17 12:17:30 +0200

    avant gitkraken glo bis

--- contents/registration.php
+++ contents/registration.php
@@ -6,7 +6,7 @@ echo "Pour vous inscrire, c'est ici";
 <div class="container">
     <h1>formulaire d'inscription</h1>
     <form  method="POST" action="./scripts/inscription.php">
-        Username : <input type="text" name="username"><br><br>
+        Username : <input id="usercheck" type="text" name="username"><br><br>
         Mot de Passe : <input type="password" name="password">
         <input type="submit" value="Envoyer" class="btn btn-success">
     </form>
